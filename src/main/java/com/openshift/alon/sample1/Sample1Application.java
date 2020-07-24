5a7556666444ff1509db8aa3908a9e7cb7e2df39
Pyths <jalon5747@gmail.com>
2020-07-24 18:06:23 -0500

    updating changes

--- src/main/java/com/openshift/alon/sample1/Sample1Application.java
+++ src/main/java/com/openshift/alon/sample1/Sample1Application.java
@@ -2,12 +2,39 @@ package com.openshift.alon.sample1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import com.google.gson.Gson;
 
 @SpringBootApplication
+@RestController
 public class Sample1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Sample1Application.class, args);
+	}
+	
+	private String appName = "Sample 1"; 
+
+	@GetMapping("/")
+	public String hello() {
+		String webContent = String.format("<h1>Welcome to %s.</h1>", appName);
+		webContent += String.format("<p>This is just a demo</p>");
+		return webContent;
+	}
+	
+	@GetMapping("/family")
+	public String getFamilyAPI() {
+		List<String> brothersList = new ArrayList<String>(Arrays.asList("Dannel", "Joel", "Eitan"));
+		HashMap<String, List<String>> familyMap = new HashMap<>();
+		familyMap.put("brothers", brothersList);
+		List<String> puppyList = new ArrayList<>(Arrays.asList("Rocky", "Cloe", "Coco"));
+		familyMap.put("puppies", puppyList);
+		return new Gson().toJson(familyMap);
 	}
 
 }
